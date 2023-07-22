6f3e18d2878c23d8b0ac793064cd4cd97571200d
Yan Yan <cobrayy@gmail.com>
2023-07-22 16:11:18 +0800

    Update main.py
    source change test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import importlib.util
 import folder_paths
 import time
 
+# ####
 def execute_prestartup_script():
     def execute_script(script_path):
         module_name = os.path.splitext(script_path)[0]
